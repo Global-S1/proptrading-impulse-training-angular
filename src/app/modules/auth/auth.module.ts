1c5a257ab4aaebd50f5f3b91758497985d40c856
Luis Saravia <luis.saravia@globals.one>
2023-11-01 22:15:55 -0500

    build(Login): Construcción de maquetado completa

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AuthComponent, FooterComponent],
+  declarations: [AuthComponent, FooterComponent, LoginComponent],
   imports: [
     CommonModule
   ],
