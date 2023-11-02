238a56a4ee5971067f6a0fd1d93a576c851cdaa2
Luis Saravia <luis.saravia@globals.one>
2023-11-02 10:24:17 -0500

    build(shared): Creación librería componentes, cambio de componentes en login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { CoreModule } from 'src/app/shared/core/core.module';
 
 @NgModule({
   declarations: [AuthComponent, FooterComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule
   ],
   exports: [AuthComponent]
 })
