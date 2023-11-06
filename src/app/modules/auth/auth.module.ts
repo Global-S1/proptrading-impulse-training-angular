5c885c0f4cbabf9bf4259ddb7afd58503f7c12db
Luis Saravia <luis.saravia@globals.one>
2023-11-06 11:03:06 -0500

    build(response-success): construcción página response-success

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { LoginComponent } from './login/login.component';
 import { CoreModule } from 'src/app/shared/core/core.module';
 import { RecoveryPasswordComponent } from './recovery-password/recovery-password.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ResponseSuccessComponent } from './response-success/response-success.component';
 
 @NgModule({
-  declarations: [AuthComponent, FooterComponent, LoginComponent, RecoveryPasswordComponent, ChangePasswordComponent],
+  declarations: [AuthComponent, FooterComponent, LoginComponent, RecoveryPasswordComponent, ChangePasswordComponent, ResponseSuccessComponent],
   imports: [
     CommonModule,
     CoreModule
