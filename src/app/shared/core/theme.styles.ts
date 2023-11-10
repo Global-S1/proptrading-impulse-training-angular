38f014b67b09a7874939905275f4b70a16b636ea
Daniel Boggiano Sáenz <58956694+danielboggianosa@users.noreply.github.com>
2023-11-09 23:33:10 -0500

    Revert "feat(Offers): Vista Offers (#11)"
    This reverts commit 0653a99808063c8a092eab6d62550a4d5fb71dc4.

--- src/app/shared/core/theme.styles.ts
+++
@@ -1,9 +0,0 @@
-import { IThemeBtn } from "./theme.styles.interface";
-
-export const ThemeBtn: IThemeBtn = {
-  min: 'min',
-  page: 'page',
-  pageAnalyzer: 'page-analyzer',
-  pagePromotion: 'page-promotion',
-  default: ''
-}
