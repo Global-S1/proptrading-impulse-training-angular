e91dcaf473475c08b29707a9f68b38730f5eeb93
Luis Saravia <luis.saravia@globals.one>
2023-11-14 17:58:03 -0500

    test(promotion): Añadiendo test a promotion

--- src/app/modules/promotion/promotion.component.spec.ts
+++ src/app/modules/promotion/promotion.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PromotionComponent } from './promotion.component';
+import { CoreModule } from 'src/app/shared/core/core.module';
 
-describe('PromotionComponent', () => {
+fdescribe('PromotionComponent', () => {
   let component: PromotionComponent;
   let fixture: ComponentFixture<PromotionComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PromotionComponent]
+      declarations: [PromotionComponent],
+      imports: [CoreModule]
     });
     fixture = TestBed.createComponent(PromotionComponent);
     component = fixture.componentInstance;
@@ -17,5 +19,31 @@ describe('PromotionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an image with alt text "ticket"', () => {
+    const imgElement: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(imgElement).toBeTruthy();
+    expect(imgElement.alt).toBe('ticket');
+  });
+
+  it('should have a heading with text "Cuenta Promocional"', () => {
+    const headingElement: HTMLHeadingElement = fixture.nativeElement.querySelector('h3');
+    expect(headingElement).toBeTruthy();
+    expect(headingElement.textContent).toContain('Cuenta Promocional');
+  });
+
+  it('should have a paragraph with promotional code instructions', () => {
+    const paragraphElement: HTMLParagraphElement = fixture.nativeElement.querySelector('p');
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.textContent).toContain('Ingresa el código promocional que te fue brindado para obtener tu desafío');
+  });
+
+
+  it('should have a button with text "VALIDAR CÓDIGO" and theme "pagePromotion"', () => {
+    const buttonElement: HTMLButtonElement = fixture.nativeElement.querySelector('app-button');
+    expect(buttonElement).toBeTruthy();
+    expect(buttonElement.textContent).toContain('VALIDAR CÓDIGO');
+    expect(buttonElement.getAttribute('theme')).toBe('pagePromotion');
   });
 });
