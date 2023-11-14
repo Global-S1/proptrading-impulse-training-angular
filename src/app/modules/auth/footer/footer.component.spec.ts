e1790422ad6d0ad3c87f0374202f3002abe9660f
Luis Saravia <luis.saravia@globals.one>
2023-11-14 12:23:12 -0500

    test(footer): Añadiendo testing al footer

--- src/app/modules/auth/footer/footer.component.spec.ts
+++ src/app/modules/auth/footer/footer.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
 
-describe('FooterComponent', () => {
+fdescribe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
@@ -17,5 +18,39 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the copyright text', () => {
+    const copyrightElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(copyrightElement.textContent).toContain('© Impulse World 2023. Todos los derechos reservados.');
+  });
+
+  it('should have the correct number of links', () => {
+    const linkElements = fixture.debugElement.queryAll(By.css('a'));
+    expect(linkElements.length).toBe(3);
+  });
+
+  it('should have the correct href attribute for each link', () => {
+    const linkElements = fixture.debugElement.queryAll(By.css('a'));
+
+    const expectedHrefs = [
+      'https://impulseworld.pro/',
+      'https://impulseworld.pro/desafios/',
+      'https://impulseworld.pro/terminos-y-condiciones/',
+    ];
+
+    linkElements.forEach((link, index) => {
+      const hrefAttribute = link.nativeElement.getAttribute('href');
+      expect(hrefAttribute).toBe(expectedHrefs[index]);
+    });
+  });
+
+  it('should open links in a new tab', () => {
+    const linkElements = fixture.debugElement.queryAll(By.css('a'));
+
+    linkElements.forEach((link) => {
+      const targetAttribute = link.nativeElement.getAttribute('target');
+      expect(targetAttribute).toBe('_blank');
+    });
   });
 });
