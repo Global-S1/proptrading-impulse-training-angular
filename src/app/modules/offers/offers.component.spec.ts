f47e044d07b9a359addd7c3f2534ad9bf3414eda
Luis Saravia <luis.saravia@globals.one>
2023-11-15 08:28:37 -0500

    test(offers): Añadiendo Test a vista Offers

--- src/app/modules/offers/offers.component.spec.ts
+++ src/app/modules/offers/offers.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OffersComponent } from './offers.component';
+import { CoreModule } from 'src/app/shared/core/core.module';
 
-describe('OffersComponent', () => {
+fdescribe('OffersComponent', () => {
   let component: OffersComponent;
   let fixture: ComponentFixture<OffersComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [OffersComponent]
+      declarations: [OffersComponent],
+      imports: [CoreModule]
     });
     fixture = TestBed.createComponent(OffersComponent);
     component = fixture.componentInstance;
@@ -17,5 +19,29 @@ describe('OffersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a div containing challenge information', () => {
+    const challengeDiv: HTMLDivElement = fixture.nativeElement.querySelector('div > div');
+    expect(challengeDiv).toBeTruthy();
+
+    const headingElement: HTMLHeadingElement = fixture.nativeElement.querySelector('h3');
+    expect(headingElement).toBeTruthy();
+    expect(headingElement.textContent).toContain('Comprar ticket Elevate');
+
+    const paragraphElement: HTMLParagraphElement = fixture.nativeElement.querySelector('p');
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement.textContent).toContain('Ingresa el código promocional que te fue brindado para obtener tu desafío');
+
+    const buttonElement: HTMLButtonElement = fixture.nativeElement.querySelector('app-button');
+    expect(buttonElement).toBeTruthy();
+    expect(buttonElement.textContent).toContain('COMPRAR TICKET');
+    expect(buttonElement.getAttribute('theme')).toBe('pagePromotion');
+  });
+
+  it('should have an image with alt text "offers"', () => {
+    const imgElement: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(imgElement).toBeTruthy();
+    expect(imgElement.alt).toBe('offers');
   });
 });
