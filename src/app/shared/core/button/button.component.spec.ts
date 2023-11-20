2888593e7745116db6af39e82dac9ada089def43
Luis Saravia <luis.saravia@globals.one>
2023-11-20 09:55:34 -0500

    test(app-button): añadiendo testing

--- src/app/shared/core/button/button.component.spec.ts
+++ src/app/shared/core/button/button.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
 
-describe('ButtonComponent', () => {
+fdescribe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
 
@@ -17,5 +17,24 @@ describe('ButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('test at dinamic text ', () => {
+    const btnElement = document.querySelector('button');
+
+    (btnElement as HTMLButtonElement).textContent = "Probando botón";
+    fixture.detectChanges();
+
+    expect(btnElement?.textContent).toBe("Probando botón");
+  });
+
+  it('test at type', () => {
+    const btnElement = document.querySelector('button');
+    expect(btnElement?.type).toBe("submit");
+
+    (btnElement as HTMLButtonElement).type = "button";
+    fixture.detectChanges();
+
+    expect(btnElement?.type).toBe("button");
   });
 });
