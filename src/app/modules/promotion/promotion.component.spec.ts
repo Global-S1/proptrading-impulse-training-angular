3442c03ef9e432fd45ae1ceaa255d724ebc0585d
Luis Saravia <luis.saravia@globals.one>
2023-11-24 21:16:01 -0500

    refactor(promotion): agregando al titulo de los test el 'when'

--- src/app/modules/promotion/promotion.component.spec.ts
+++ src/app/modules/promotion/promotion.component.spec.ts
@@ -21,26 +21,26 @@ fdescribe('PromotionComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have an image with alt text "ticket"', () => {
+  it('should display an image with alt text "ticket", when initialized', () => {
     const imgElement: HTMLImageElement = fixture.nativeElement.querySelector('img');
     expect(imgElement).toBeTruthy();
     expect(imgElement.alt).toBe('ticket');
   });
 
-  it('should have a heading with text "Cuenta Promocional"', () => {
+  it('should display a heading with text "Cuenta Promocional", when initialized', () => {
     const headingElement: HTMLHeadingElement = fixture.nativeElement.querySelector('h3');
     expect(headingElement).toBeTruthy();
     expect(headingElement.textContent).toContain('Cuenta Promocional');
   });
 
-  it('should have a paragraph with promotional code instructions', () => {
+  it('should display a paragraph with promotional code instructions, when initialized', () => {
     const paragraphElement: HTMLParagraphElement = fixture.nativeElement.querySelector('p');
     expect(paragraphElement).toBeTruthy();
     expect(paragraphElement.textContent).toContain('Ingresa el código promocional que te fue brindado para obtener tu desafío');
   });
 
 
-  it('should have a button with text "VALIDAR CÓDIGO" and theme "pagePromotion"', () => {
+  it('should display a button with text "VALIDAR CÓDIGO" and theme "pagePromotion", when initialized', () => {
     const buttonElement: HTMLButtonElement = fixture.nativeElement.querySelector('app-button');
     expect(buttonElement).toBeTruthy();
     expect(buttonElement.textContent).toContain('VALIDAR CÓDIGO');
